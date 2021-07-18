169d18f71ec547cad2a670171120a6d59261ca30
ksqsf <i@ksqsf.moe>
2021-07-18 22:36:38 +0800

    add nix

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs; stdenv.mkDerivation {
+  name = "cll";
+  buildInputs = [
+    xmlto
+    ruby_3_0
+    rubyPackages_3_0.nokogiri
+  ];
+  shellHook = ''
+      gem install optimist
+      gem install htmlentities
+  '';
+}
